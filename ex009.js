f23b18429298a69cc90869eb348780ff5d9c6055
CarlosAugusto <carlosferreirab25@gmail.com>
2021-06-21 06:37:44 -0300

    tipoDeCondicoesJavaScript

---
+++ ex009.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+console.log('Você esta vivendo em ', pais)
+if (pais != 'Brasil'){
+    console.log('Vc é estrageiro.')
+}
+else{
+    console.log('Você é Brasileiro.')
+}
---
+++ exoo8.js
@@ -0,0 +1,8 @@
+var vel = 55
+console.log('Sua velocidade é de', vel,'Km')
+if ( vel >= 61){
+    console.log('Parabens vc acaba de ser multado!')
+
+}else{
+    console.log('Continue assim.')
+}
